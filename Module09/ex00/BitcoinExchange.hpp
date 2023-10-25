b07934e11b23aaaa0ac1489ade6836f6a77bf784
42MacAntoine <amontalb@student.42lyon.fr>
2023-10-25 13:58:43 +0200

    cpp09 begin

--- Module09/ex00/BitcoinExchange.hpp
+++ Module09/ex00/BitcoinExchange.hpp
@@ -1,4 +1,12 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+#include <cstdlib>
 
+using std::cout;
+using std::endl;
+using std::string;
+using std::map;
--- Module09/ex00/main.cpp
+++ Module09/ex00/main.cpp
@@ -1,1 +1,46 @@
 #include "BitcoinExchange.hpp"
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+		return 1;
+	
+
+	//mapping 
+
+	map<string, double> database;
+
+	std::ifstream datafile("data.csv");
+
+    if (!datafile.is_open()) {
+        std::cerr << "Error: Could not open the file." << std::endl;
+        return 1;
+    }
+
+	string line;
+	std::getline(datafile, line);
+    while (std::getline(datafile, line)) 
+	{
+		int commaPlace = line.find(",");
+		string date = line.substr(0, commaPlace);
+		char* end;
+		double price = std::strtod(line.substr(commaPlace + 1, line.size()).c_str(), &end);
+		// cout << date << "   " << price << endl;
+		database.insert(std::make_pair(date, price));
+	}
+	datafile.close();
+
+	// answer
+
+	std::ifstream file(argv[1]);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open the file." << std::endl;
+        return 1;
+    }
+	
+
+
+
+	return 0 ;
+}
